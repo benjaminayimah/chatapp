85e64d746df9125d9d884debab8a3712735dbac0
Benjamin <benjaminayimah@gmail.com>
2024-10-05 20:29:06 +0000

    commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,6 @@
 <template>
     <nav class="sticky sticky-top bg-surface-1">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/gemini-pro">Gemini-Pro</router-link> |
-        <router-link to="/vision-pro">Vision-Pro</router-link> |
-        <router-link to="/gemini-start">Gemini Start</router-link>
+        
     </nav>
 </template>
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GeminiPro from '@/views/GeminiPro.vue'
-import VisionPro from '@/views/VisionPro.vue'
-import GeminiStart from '@/views/GeminiStart.vue'
-// import Settings from '@/modals/SettingsModal.vue'
-
-
-
 
 
 const routes = [
   { path: '/', name: 'home', component: HomeView},
-  { path: '/gemini-pro', name: 'GeminiPro', component: GeminiPro},
-  { path: '/vision-pro', name: 'VisionPro', component: VisionPro},
-  { path: '/gemini-start', name: 'GeminiStart', component: GeminiStart},
-
-  
-
+  { path: '/gemini-pro', name: 'GeminiPro', component: GeminiPro}
 
 
 ]
