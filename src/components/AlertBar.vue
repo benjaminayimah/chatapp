db0a8002f6493a57044fa1099d4965f69fb4bd96
Benjamin <benjaminayimah@gmail.com>
2024-12-03 21:51:32 +0000

    commit

--- src/components/AlertBar.vue
+++ src/components/AlertBar.vue
@@ -1,13 +1,13 @@
 <template>
   <transition name="slide">
     <div v-if="alert" class="alert-container">
-      <div class="error-snack-container bg-modal-surface">
+      <div class="error-snack-container bg-surface-inverse ">
         <div class="flex ai-c gap-10" role="alert">
           <span v-html="icon(this.alert.type)" class="flex"></span>
           <div class="flex-1 text-left">{{ alert.message }}</div>
-          <button @mousedown="$store.commit('dismissAlert')" class="transparent-button jc-c ai-c flex-shrink-0" data-type="modal">
+          <button @mousedown="$store.commit('dismissAlert')" class="bg-transparent btn-inverted jc-c ai-c flex-shrink-0" data-type="modal">
               <svg height="12" viewBox="0 0 13.587 13.587">
-                  <path id="close_modal" d="M7.163,19.188,5.8,17.83,11.239,12.4,5.8,6.96,7.163,5.6,12.6,11.036,18.033,5.6,19.392,6.96,13.957,12.4l5.435,5.435-1.359,1.359L12.6,13.754Z" transform="translate(-5.805 -5.602)"/>
+                  <path id="close_alert" d="M7.163,19.188,5.8,17.83,11.239,12.4,5.8,6.96,7.163,5.6,12.6,11.036,18.033,5.6,19.392,6.96,13.957,12.4l5.435,5.435-1.359,1.359L12.6,13.754Z" transform="translate(-5.805 -5.602)"/>
               </svg>
           </button>
         </div>
@@ -52,7 +52,7 @@ export default {
 .alert-container {
   position: fixed;
   z-index: 9999;
-  top: 10px;
+  top: 7px;
   width: 100%;
   text-align: center;
   padding: 0 20px;
@@ -69,6 +69,9 @@ export default {
 button {
   height: 32px;
   width: 32px;
+  &:hover {
+    background-color: var(--alert-close-button-hover);
+  }
 }
 .slide-enter-active {
   transition: all 0.4s;
